b7c3410c73fe0c3ae40f9d966b69818a5b0fa6fb
B.A. Imambaks Rodrigues <0924777@hr.nl>
2021-01-27 20:11:01 +0100

    HTMLLL DONEEEEEEEEEEEEEEEEE

--- bayvinemedia/src/components/Form/Form.js
+++ bayvinemedia/src/components/Form/Form.js
@@ -7,16 +7,32 @@ const Form = () => {
 			<h1>Contact</h1>
 			<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </span>
 			<form>
-				<label for="name">Name</label>
-				<input name="name" type="text" />
-				<label for="email">Email</label>
-				<input name="email" type="email" />
-				<label for="organisation">Organisation</label>
-				<input name="organisation" type="text" />
-				<label for="phone">Phone</label>
-				<input name="phone" type="tel" />
-				<label for="message">Message</label>
-				<input name="message" type="textarea" />
+				<div>
+					<label for="name">Name</label>
+					<input name="name" type="text" />
+				</div>
+
+				<div>
+					<label for="email">Email</label>
+					<input name="email" type="email" />
+				</div>
+
+				<div>
+					<label for="organisation">Organisation</label>
+					<input name="organisation" type="text" />
+				</div>
+
+				<div>
+					<label for="phone">Phone</label>
+					<input name="phone" type="tel" />
+				</div>
+
+				<div>
+					<label for="message">Message</label>
+
+					<textarea></textarea>
+				</div>
+
 				<button type="submit">S E N D</button>
 			</form>
 		</div>
