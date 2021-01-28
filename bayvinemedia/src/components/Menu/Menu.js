76fb7f2ac49ed0025c71aac49f886835ed9b601a
B.A. Imambaks Rodrigues <0924777@hr.nl>
2021-01-28 04:43:06 +0100

    menu done

--- bayvinemedia/src/components/Menu/Menu.js
+++ bayvinemedia/src/components/Menu/Menu.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react"
 import "./Menu.scss"
+import sanfran from "../../media/menubackgroundsan.png"
+import statue from "../../media/statue.jpg"
+import beach from "../../media/beach.jpg"
+import together from "../../media/together.jpg"
 
 const Menu = (props) => {
 	let attachedClasses = ["menu", "closed"]
 	let attachedClasses1 = ["menu2", "closed"]
 	let attachedClasses2 = ["menu3", "closed"]
+	let imageClass = ""
 
 	let [currentImage, setcurrentImage] = useState("")
 
@@ -14,25 +19,57 @@ const Menu = (props) => {
 		attachedClasses2 = ["menu3", "open"]
 	}
 
+	const backgroundHandler = (image) => {
+		setcurrentImage(image)
+		imageClass = "kenburn"
+	}
+
+	const resetImage = () => {
+		imageClass = ""
+		setcurrentImage("")
+	}
+
 	return (
 		<>
 			<div className={attachedClasses.join(" ")}>
 				<div className="menu-items">
 					<ul>
 						<li>
-							<h1>About</h1>
+							<h1
+								onMouseLeave={resetImage}
+								onMouseOver={() => backgroundHandler(sanfran)}
+							>
+								About
+							</h1>
 						</li>
 						<li>
-							<h1>Services</h1>
+							<h1
+								onMouseLeave={resetImage}
+								onMouseOver={() => backgroundHandler(statue)}
+							>
+								Services
+							</h1>
 						</li>
 						<li>
-							<h1>Reviews</h1>
+							<h1
+								onMouseLeave={resetImage}
+								onMouseOver={() => backgroundHandler(beach)}
+							>
+								Reviews
+							</h1>
 						</li>
 						<li>
-							<h1>Contact Me</h1>
+							<h1
+								onMouseLeave={resetImage}
+								onMouseOver={() => backgroundHandler(together)}
+							>
+								Contact Me
+							</h1>
 						</li>
 					</ul>
 				</div>
+
+				<img className={imageClass} src={currentImage} />
 			</div>
 			<div className={attachedClasses1.join(" ")}></div>
 			<div className={attachedClasses2.join(" ")}></div>
