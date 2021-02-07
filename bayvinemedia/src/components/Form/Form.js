b82bf1656f4fa254175647c6b87c29912f66ba70
B.A. Imambaks Rodrigues <0924777@hr.nl>
2021-02-07 20:04:04 +0100

    build with form handler

--- bayvinemedia/src/components/Form/Form.js
+++ bayvinemedia/src/components/Form/Form.js
@@ -1,14 +1,50 @@
 import React, { useState } from "react"
 import "./Form.scss"
 
+function encode(data) {
+	return Object.keys(data)
+		.map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+		.join("&")
+}
+
 const Form = () => {
 	const [loader, setLoader] = useState(false)
 	const [success, setSuccess] = useState(false)
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		organization: "",
+		phone: "",
+		message: "",
+	})
 
 	const formHandler = (e) => {
-		setLoader(true)
+		fetch("/", {
+			method: "POST",
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
+			body: encode({ "form-name": "contact", ...formData }),
+		})
+			.then(() => alert("Success!"))
+			.catch((error) => alert(error))
+
 		e.preventDefault()
-		console.log("Submit form.")
+	}
+
+	const setForm = (e, message) => {
+		let name = e.target.name
+		let value = e.target.value
+
+		if (message) {
+			setFormData({
+				...formData,
+				message: value,
+			})
+		} else {
+			setFormData({
+				...formData,
+				[name]: value,
+			})
+		}
 	}
 
 	return (
@@ -22,37 +58,59 @@ const Form = () => {
 				<form
 					name="contact"
 					method="post"
-					netlify
+					netlify="true"
 					netlify-honeybot="bot-field"
-					onSubmit="submit"
+					onSubmit={formHandler}
 				>
 					<input type="hidden" name="form-name" value="contact" />
 					<div>
 						<label htmlFor="name">Name</label>
-						<input name="name" type="text" />
+						<input
+							onChange={setForm}
+							value={formData.name}
+							name="name"
+							type="text"
+						/>
 					</div>
 
 					<div>
 						<label htmlFor="email">Email</label>
-						<input name="email" type="email" />
+						<input
+							value={formData.email}
+							onChange={setForm}
+							name="email"
+							type="email"
+						/>
 					</div>
 
 					<div>
-						<label htmlFor="organisation">Organisation</label>
-						<input name="organisation" type="text" />
+						<label htmlFor="organization">Organization</label>
+						<input
+							value={formData.organization}
+							onChange={setForm}
+							name="organization"
+							type="text"
+						/>
 					</div>
 
 					<div>
 						<label htmlFor="phone">Phone</label>
-						<input name="phone" type="tel" />
+						<input
+							value={formData.phone}
+							onChange={setForm}
+							name="phone"
+							type="tel"
+						/>
 					</div>
 
 					<div>
 						<label htmlFor="message">Message</label>
-						<textarea></textarea>
+						<textarea onChange={(event) => setForm(event, true)} />
 					</div>
 
-					<button type="submit">S E N D</button>
+					<button type="submit" onClick={formHandler}>
+						S E N D
+					</button>
 				</form>
 			)}
 		</div>
