5cedf50502a1e5ccda122df8aa505d19c9d0018c
B.A. Imambaks Rodrigues <0924777@hr.nl>
2021-02-07 19:36:23 +0100

    z

--- bayvinemedia/src/components/Form/Form.js
+++ bayvinemedia/src/components/Form/Form.js
@@ -1,46 +1,56 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Form.scss"
 
 const Form = () => {
+	const [loader, setLoader] = useState(false)
+	const [success, setSuccess] = useState(false)
+
 	const formHandler = (e) => {
+		setLoader(true)
 		e.preventDefault()
 		console.log("Submit form.")
 	}
+
 	return (
 		<div className="form">
 			<h1>Contact</h1>
 			<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </span>
-			<form name="contact" method="post" data-netlify="true">
-				<input type="hidden" name="form-name" value="contact" />
-				<div>
-					<label htmlFor="name">Name</label>
-					<input name="name" type="text" />
-				</div>
 
-				<div>
-					<label htmlFor="email">Email</label>
-					<input name="email" type="email" />
-				</div>
+			{loader ? (
+				<span>Sending form...</span>
+			) : (
+				<form name="contact" method="post" netlify netlify-honeybot="bot-field">
+					<input type="hidden" name="form-name" value="contact" />
+					<div>
+						<label htmlFor="name">Name</label>
+						<input name="name" type="text" />
+					</div>
 
-				<div>
-					<label htmlFor="organisation">Organisation</label>
-					<input name="organisation" type="text" />
-				</div>
+					<div>
+						<label htmlFor="email">Email</label>
+						<input name="email" type="email" />
+					</div>
 
-				<div>
-					<label htmlFor="phone">Phone</label>
-					<input name="phone" type="tel" />
-				</div>
+					<div>
+						<label htmlFor="organisation">Organisation</label>
+						<input name="organisation" type="text" />
+					</div>
 
-				<div>
-					<label htmlFor="message">Message</label>
-					<textarea></textarea>
-				</div>
+					<div>
+						<label htmlFor="phone">Phone</label>
+						<input name="phone" type="tel" />
+					</div>
 
-				<button type="submit" onClick={(e) => formHandler(e)}>
-					S E N D
-				</button>
-			</form>
+					<div>
+						<label htmlFor="message">Message</label>
+						<textarea></textarea>
+					</div>
+
+					<button type="submit" onClick={(e) => formHandler(e)}>
+						S E N D
+					</button>
+				</form>
+			)}
 		</div>
 	)
 }
