a892ca44bbe008b64789d98b29e8adef49df159d
Brainilio <brainilioir@gmail.com>
2023-01-23 13:49:11 -0800

    Add Prismic

---
+++ .slicemachine/assets/slices/IntroSection/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/IntroSection';
+
+export default {
+  title: 'slices/IntroSection'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Child","spans":[]}],"description":[{"type":"paragraph","text":"Pariatur non est est eiusmod deserunt ullamco dolor adipisicing. Officia exercitation laboris ad proident.","spans":[]}]},"id":"_Default","slice_type":"intro_section"}} />
+_Default.storyName = ''
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css"
+import { PrismicProvider } from "@prismicio/react"
+import { PrismicPreview } from "@prismicio/next"
+import { repositoryName } from "../../prismicio"
+import Link from "next/link"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
+			<PrismicPreview repositoryName={repositoryName}>
+				<Component {...pageProps} />
+			</PrismicPreview>
+		</PrismicProvider>
+	)
 }
