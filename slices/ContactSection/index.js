3cdd42e274d77f9f159102610de3d09fb760e670
Brainilio <brainilioir@gmail.com>
2023-02-23 13:13:01 -0800

    tewst-date

--- slices/ContactSection/index.js
+++ slices/ContactSection/index.js
@@ -12,14 +12,58 @@ const ContactSection = ({ slice }) => {
 	const title = prismicH.asText(slice.primary.title)
 	const description = slice.primary.description
 
-	const htmlSerializer = (type, element, text, children) => {}
+	// const htmlSerializer = (type, element, text, children) => {
+	//   return {
+	//     hyperlink: ()
+	//   }
+	// }
 
 	return (
 		<section className="text-white">
 			<Title title={title} />
 			<div className="px-5">
-				<PrismicRichText field={description} className="underline" />
+				<PrismicRichText
+					field={description}
+					className="underline"
+					components={{
+						hyperlink: (props) => (
+							<PrismicLink
+								className="underline"
+								{...props}
+								href={props.node.data.url}
+							>
+								{props.text}
+							</PrismicLink>
+						),
+					}}
+				/>
 			</div>
+			<form>
+				{/* First row */}
+				<div>
+					{/* First name, not required */}
+					<div></div>
+					{/* Email, required */}
+					<div></div>
+				</div>
+				{/* Second row */}
+				<div>
+					{/* Phone number, not required */}
+					<div></div>
+					{/* Project budget, not required */}
+					<div></div>
+				</div>
+				{/* Third row */}
+				<div>
+					{/* Tell us about the project */}
+					<div></div>
+				</div>
+				{/* Fourth row */}
+				<div>
+					{/* Submit button */}
+					<div></div>
+				</div>
+			</form>
 		</section>
 	)
 }
