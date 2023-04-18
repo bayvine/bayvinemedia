ddc16af3d21766ae2f4a4ee5ad678f1c8614b177
Brainilio <brainilioir@gmail.com>
2023-04-18 13:10:11 -0700

    test

--- slices/ContactSection/index.js
+++ slices/ContactSection/index.js
@@ -28,6 +28,8 @@ const ContactSection = ({ slice }) => {
 
 	const [loading, setLoading] = useState(false)
 
+	const [formSuccess, setFormSuccess] = useState(false)
+
 	const OPTIONS = [
 		"$1,000 - $3,000",
 		"$3,000 - $5,000",
@@ -68,13 +70,44 @@ const ContactSection = ({ slice }) => {
 		}
 	}
 
+	function encode(data) {
+		return Object.keys(data)
+			.map(
+				(key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+			)
+			.join("&")
+	}
+
 	const handleFormSubmission = (e) => {
 		console.log(name, email, phoneNumber, budget, message)
 		e.preventDefault()
-		wipeErrors()
 		setLoading(true)
-
 		const isValid = validateForm()
+
+		if (isValid) {
+			fetch("/", {
+				method: "POST",
+				headers: { "Content-Type": "application/x-www-form-urlencoded" },
+				body: encode({
+					"form-name": "contact",
+					name: name,
+					email: email,
+					phone: phoneNumber,
+					budget: budget,
+					message: message,
+				}),
+			})
+				.then(() => {
+					wipeErrors()
+					setLoading(false)
+					setFormSuccess(true)
+					wipeForm()
+					console.log("Form successfully submitted")
+				})
+				.catch((error) => setLoading(false))
+		}
+
+		setLoading(false)
 	}
 
 	return (
@@ -97,100 +130,113 @@ const ContactSection = ({ slice }) => {
 					}}
 				/>
 			</div>
-			<form
-				className="grid grid-cols-1 px-5 text-black"
-				onSubmit={handleFormSubmission}
-			>
-				{/* First row */}
-				<div className="">
-					{/* First name, not required */}
-					<div className="flex flex-col mt-4">
-						<label className="pb-2 text-white">Name*:</label>
-						<input
-							type="text"
-							placeholder="John Doe..."
-							required
-							onInput={(e) => setName(e.target.value)}
-							className="px-3 py-3 text-white bg-transparent border rounded-md"
-						></input>
-						{nameErr && (
-							<div className="py-0 mt-1 text-sm text-red-400">
-								Please provide a name!
-							</div>
-						)}
-					</div>
-					{/* Email, required */}
-					<div className="flex flex-col mt-4">
-						<label className="pb-2 text-white">Email*:</label>
-						<input
-							placeholder="John@doe.com..."
-							type="email"
-							required
-							onInput={(e) => setEmail(e.target.value)}
-							className="px-3 py-3 text-white bg-transparent border rounded-md"
-						></input>
-						{emailErr && (
-							<div className="py-0 mt-1 text-sm text-red-400">
-								Please provide a valid email!
-							</div>
-						)}
-					</div>
+
+			{formSuccess ? (
+				<div>
+					Thank you for submitting the form! We'll get back to you as soon as
+					possible.
 				</div>
-				{/* Second row */}
-				<div className="">
-					{/* Phone number, not required */}
-					<div className="flex flex-col mt-4">
-						<label className="pb-2 text-white">Phone:</label>
-						<input
-							onInput={(e) => setPhoneNumber(e.target.value)}
-							type="text"
-							placeholder="(408)-123-4567"
-							className="px-3 py-3 text-white bg-transparent border rounded-md"
-						></input>
+			) : loading ? (
+				<div>Submitting...</div>
+			) : (
+				<form
+					className="grid grid-cols-1 px-5 text-black"
+					onSubmit={handleFormSubmission}
+					name="contact"
+					method="POST"
+					data-netlify="true"
+				>
+					{/* First row */}
+					<div className="">
+						{/* First name, not required */}
+						<div className="flex flex-col mt-4">
+							<label className="pb-2 text-white">Name*:</label>
+							<input
+								type="text"
+								placeholder="John Doe..."
+								required
+								onInput={(e) => setName(e.target.value)}
+								className="px-3 py-3 text-white bg-transparent border rounded-md"
+							></input>
+							{nameErr && (
+								<div className="py-0 mt-1 text-sm text-red-400">
+									Please provide a name!
+								</div>
+							)}
+						</div>
+						{/* Email, required */}
+						<div className="flex flex-col mt-4">
+							<label className="pb-2 text-white">Email*:</label>
+							<input
+								placeholder="John@doe.com..."
+								type="email"
+								required
+								onInput={(e) => setEmail(e.target.value)}
+								className="px-3 py-3 text-white bg-transparent border rounded-md"
+							></input>
+							{emailErr && (
+								<div className="py-0 mt-1 text-sm text-red-400">
+									Please provide a valid email!
+								</div>
+							)}
+						</div>
 					</div>
-					{/* Project budget, not required */}
-					<div className="flex flex-col mt-4">
-						<label className="pb-2 text-white">Project budget:</label>
-						<select
-							onInput={(e) => setBudget(e.target.value)}
-							value={budget}
-							className="px-3 py-3 text-white bg-transparent border rounded-md"
-						>
-							<option disabled value={"Select your budget"}>
-								Select your budget
-							</option>
-							{OPTIONS.map((item, index) => (
-								<option key={index}>{item}</option>
-							))}
-						</select>
+					{/* Second row */}
+					<div className="">
+						{/* Phone number, not required */}
+						<div className="flex flex-col mt-4">
+							<label className="pb-2 text-white">Phone:</label>
+							<input
+								onInput={(e) => setPhoneNumber(e.target.value)}
+								type="text"
+								placeholder="(408)-123-4567"
+								className="px-3 py-3 text-white bg-transparent border rounded-md"
+							></input>
+						</div>
+						{/* Project budget, not required */}
+						<div className="flex flex-col mt-4">
+							<label className="pb-2 text-white">Project budget:</label>
+							<select
+								onInput={(e) => setBudget(e.target.value)}
+								value={budget}
+								className="px-3 py-3 text-white bg-transparent border rounded-md"
+							>
+								<option disabled value={"Select your budget"}>
+									Select your budget
+								</option>
+								{OPTIONS.map((item, index) => (
+									<option key={index}>{item}</option>
+								))}
+							</select>
+						</div>
 					</div>
-				</div>
-				{/* Third row */}
-				<div className="">
-					{/* Tell us about the project */}
-					<div className="flex flex-col mt-4">
-						<label className="pb-2 text-white">
-							Tell us about your project:
-						</label>
-						<textarea
-							onInput={(e) => setMessage(e.target.value)}
-							placeholder="Mobile application, Social Media, Multiple Page Website..."
-							className="px-3 py-3 text-white bg-transparent border rounded-md"
-						></textarea>
+					{/* Third row */}
+					<div className="">
+						{/* Tell us about the project */}
+						<div className="flex flex-col mt-4">
+							<label className="pb-2 text-white">
+								Tell us about your project:
+							</label>
+							<textarea
+								onInput={(e) => setMessage(e.target.value)}
+								placeholder="Mobile application, Social Media, Multiple Page Website..."
+								className="px-3 py-3 text-white bg-transparent border rounded-md"
+							></textarea>
+						</div>
 					</div>
-				</div>
-				{/* Fourth row */}
-				<div className="mt-6">
-					{/* Submit button */}
-					<div className="w-full">
-						<button className="w-full px-8 py-2 uppercase bg-white rounded-md text-bold">
-							<div className="flex items-center justify-center font-black text-center text-black">
-								Submit
-							</div>
-						</button>
+					{/* Fourth row */}
+					<div className="mt-6">
+						{/* Submit button */}
+						<div className="w-full">
+							<button className="w-full px-8 py-2 uppercase bg-white rounded-md text-bold">
+								<div className="flex items-center justify-center font-black text-center text-black">
+									Submit
+								</div>
+							</button>
+						</div>
 					</div>
-				</div>
-			</form>
+				</form>
+			)}
 		</section>
 	)
 }
