c9952ccf77e000cf9434bb2d4617d4ebfe344bbd
Brainilio <brainilioir@gmail.com>
2025-11-17 11:41:31 -0700

    New prismic

--- old/.slicemachine/assets/slices/AboutSection/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/AboutSection';
-
-export default {
-  title: 'slices/AboutSection'
-}
-
-
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Laugh","spans":[]}],"description":[{"type":"paragraph","text":"Eu ad et aliqua voluptate incididunt. Velit elit voluptate velit.","spans":[]}],"background":{"dimensions":{"width":4000,"height":4000},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1587653915936-5623ea0b949a"}},"id":"_Default","slice_type":"about_section"}} />
-_Default.storyName = ''
